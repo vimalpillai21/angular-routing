704ddb8db8f42b646a8a6143ca11e8322c43f635
Vimal Pillai <vimalpillai@goodbits.in>
2019-04-28 10:39:23 +0530

    unsubscribe manually

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit , OnDestroy{
   user: {id: number, name: string};
-
+  subscription : Subscription;
   constructor(private router: ActivatedRoute) { }
 
   ngOnInit() {
@@ -22,6 +23,9 @@ export class UserComponent implements OnInit {
        this.user.name = params['name']
      } 
    ) 
+  }
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }

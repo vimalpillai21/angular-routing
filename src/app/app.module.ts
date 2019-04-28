69cbac26573afa671841a6a81332b094db65b10c
Vimal Pillai <vimalpillai@goodbits.in>
2019-04-28 09:53:50 +0530

    user detail routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes: Routes=[
   {path: '',component:HomeComponent},
-  {path: 'users',component:UsersComponent},
+  {path: 'users',component:UserComponent},
+  {path: 'users/:id',component:UsersComponent},
   {path: 'servers',component:ServersComponent}
 
 ]
